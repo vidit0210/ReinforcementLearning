6baeef2363cd44656ac63cbf8dba4abfe495deec
vidit0210 <vidit0210@gmail.com>
2020-04-02 04:07:46 +0530

    DriverCartPole

--- Udemy_Phil/cartpoleNaiveDeepQNetwork.py
+++ Udemy_Phil/cartpoleNaiveDeepQNetwork.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch as T
+from utils import plot_learning_curve
 
 
 class LinearDeepQNetwork(nn.Module):
@@ -67,3 +68,35 @@ class Agent():
         loss.backward()
         self.Q.optimizer.step()
         self.decrement_epsilon()
+
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v1')
+    n_games = 10000
+    scores = []
+    eps_history = []
+
+    agent = Agent(lr=0.0001, input_dims=env.observation_space.shape,
+                  n_actions=env.action_space.n)
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        obs = env.reset()
+
+        while not done:
+            action = agent.choose_action(obs)
+            obs_, reward, done, info = env.step(action)
+            score += reward
+            agent.learn(obs, action, reward, obs_)
+            obs = obs_
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+        if i % 100 == 0:
+            avg_score = np.mean(scores[-100:])
+            print('episode ', i, 'score %.1f avg score %.1f epsilon %.2f' %
+                  (score, avg_score, agent.epsilon))
+
+    filename = 'cartpole_naive_dqn.png'
+    x = [i+1 for i in range(n_games)]
+    plot_learning_curve(x, scores, eps_history, filename)
