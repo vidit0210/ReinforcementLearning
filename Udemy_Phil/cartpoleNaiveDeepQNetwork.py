755fd80b0838f5accabe715814bf45e83131a106
vidit0210 <vidit0210@gmail.com>
2020-04-02 03:02:21 +0530

    Agent-Cart-Pole

--- Udemy_Phil/cartpoleNaiveDeepQNetwork.py
+++ Udemy_Phil/cartpoleNaiveDeepQNetwork.py
@@ -18,3 +18,52 @@ class LinearDeepQNetwork(nn.Module):
 
     def forward(self, state):
         layer1 = F.relu(self.fc1(state))
+        actions = self.fc2(layer1)
+        return actions
+
+
+class Agent():
+    def __init__(self, input_dims, n_actions, lr, gamma=0.99, epsilon=1.0, eps_dec=1e-5, eps_min=0.01):
+        super().__init__()
+        self.lr = lr
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_dec = eps_dec
+        self.eps_min = eps_min
+        self.action_space = [i for i in range(self.n_actions)]
+
+        self.Q = LinearDeepQNetwork(self.lr, self.n_actions, self.input_dims)
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = T.tensor(observation, dtype=T.float).to(self.Q.device)
+            actions = self.Q.forward(state)
+            action = T.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec  \
+            if self.epsilon > self.eps_min else self.eps_min
+
+    def learn(self, state, action, reward, state_):
+        self.Q.optimizer.zero_grad()
+        states = T.tensor(state, dtype=T.float).to(self.Q.device)
+        actions = T.tensor(action).to(self.Q.device)
+        rewards = T.tensor(reward).to(self.Q.device)
+        states_ = T.tensor(state_, dtype=T.float).to(self.Q.device)
+
+        q_pred = self.Q.forward(states)[actions]
+
+        q_next = self.Q.forward(states_).max()
+
+        q_target = reward + self.gamma*q_next
+
+        loss = self.Q.loss(q_target, q_pred).to(self.Q.device)
+        loss.backward()
+        self.Q.optimizer.step()
+        self.decrement_epsilon()
